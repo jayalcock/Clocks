2313c97146f39a9293f10ecc9aea94755356a007
jayalcock <jay.alcock@me.com>
2020-09-16 23:17:56 -0700

    finished PLL setup

---
+++ Clocks 1114/Sources/main.c
@@ -0,0 +1,20 @@
+#include <__cross_studio_io.h>
+#include "LPC11xx.h"
+#include "Functions.h"
+
+
+void main(void)
+{
+  int i = 1;
+
+  LPC_GPIO1->DIR |= LED;  //Set LED pin as output
+  LPC_GPIO1->DATA &= ~LED;  //Set LED pin low
+
+  while(1)
+  {
+    ledFlash();
+  }
+
+  debug_printf("hello world\n");  
+  debug_exit(0);
+}
--- Clocks 1114/targets/LPC1000/include/CMSIS/system_LPC11xx.c
+++ Clocks 1114/targets/LPC1000/include/CMSIS/system_LPC11xx.c
@@ -121,15 +121,15 @@
 */
 #define CLOCK_SETUP           1
 #define SYSCLK_SETUP          1
-#define SYSOSC_SETUP          0           //No system (external) oscillator
+#define SYSOSC_SETUP          1           //No system (external) oscillator
 #define SYSOSCCTRL_Val        0x00000000  
 #define WDTOSC_SETUP          0           //No watchdog oscillator 
 #define WDTOSCCTRL_Val        0x000000A0
 #define SYSPLLCLKSEL_Val      0x00000000  //PLL from internal osc. When using the C_CAN controller with baudrates above 100 kbit/s, the system oscillator must be selected
 #define SYSPLL_SETUP          1           //Use PLL
-#define SYSPLLCTRL_Val        0x00000032  //msel = 2, psel = 8 (12MHz clcok / 2 * 8 = 48MHz PLL)
+#define SYSPLLCTRL_Val        0x00000007  //msel = 7, psel = 0 (12MHz clcok / 1 * 7 = 84MHz PLL)
 #define MAINCLKSEL_Val        0x00000003  //Use system PLL clock output
-#define SYSAHBCLKDIV_Val      0x00000004  //Divide PLL by 1 for AHB clock (48/1 = 48MHz)
+#define SYSAHBCLKDIV_Val      0x00000001  //Divide PLL by 1 for AHB clock (84/1 = 48MHz)
 #define AHBCLKCTRL_Val        0x0001007F  //Peripheral control 
 #define SSP0CLKDIV_Val        0x00000001
 #define UARTCLKDIV_Val        0x00000001
